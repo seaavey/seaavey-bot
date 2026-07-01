c56a2a7eb1f701ddcd52621972230f40eb5250d9
seaavey <seaavey@users.noreply.github.com>
2026-07-01 18:06:38 +0700

    feat(wallpaper): define and integrate complete Wallhaven API interfaces

--- src/commands/tools/wallpaper.ts
+++ src/commands/tools/wallpaper.ts
@@ -1,6 +1,46 @@
 import { defineCommand } from "@/core/types";
 import { safeFetchJSON } from "@/utils/helper";
 
+interface IWallHavenThumbs {
+  large: string;
+  original: string;
+  small: string;
+}
+
+interface IWallHavenData {
+  id: string;
+  url: string;
+  short_url: string;
+  views: number;
+  favorites: number;
+  source: string;
+  purity: "sfw" | "sketchy" | "nsfw";
+  category: "general" | "anime" | "people";
+  dimension_x: number;
+  dimension_y: number;
+  resolution: string;
+  ratio: string;
+  file_size: number;
+  file_type: string;
+  created_at: string;
+  colors: string[];
+  path: string;
+  thumbs: IWallHavenThumbs;
+}
+
+interface IWallHavenMeta {
+  current_page: number;
+  last_page: number;
+  per_page: number;
+  total: number;
+  query: string | null;
+  seed: string | null;
+}
+
+interface IWallHaven {
+  data: IWallHavenData[];
+  meta: IWallHavenMeta;
+}
 export default defineCommand({
   name: "Wallpaper",
   alias: ["wallpaper", "wp"],
@@ -8,9 +48,7 @@ export default defineCommand({
   usage: "{prefix}wallpaper",
   tags: ["tools"],
   handler: async (_sock, msg) => {
-    const data = await safeFetchJSON<{
-      data?: Array<{ path: string; resolution: string }>;
-    }>(
+    const data = await safeFetchJSON<IWallHaven>(
       "https://wallhaven.cc/api/v1/search?categories=111&purity=100&resolutions=1080x2340&sorting=random",
     );
 
